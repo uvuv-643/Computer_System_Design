8807b5d0d47396d547779c987ca8d6500a3f1712
ITSamantha <diana.saveleva.dos@gmail.com>
2024-09-27 14:00:32 +0300

    refactor: dir changed

---
+++ lab1/lab1/states.h
@@ -0,0 +1,16 @@
+#ifndef STATE_H
+#define STATE_H
+
+enum LightState {
+    GREEN,
+    YELLOW,
+    RED,
+    GREEN_FLUSHING,
+    GREEN_SHORT,
+    YELLOW_SHORT,
+    RED_SHORT,
+    GREEN_FLUSHING_SHORT
+};
+
+
+#endif //STATE_H
